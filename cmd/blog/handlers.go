f85487495b793508fb1cfcf68ccadefd941981a5
DESKTOP-G0AVLQ7\Елена <olya.o-sar@yandex.ru>
2023-03-27 21:21:23 +0300

    f

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -1,35 +1,56 @@
 package main
 
-import {
-	"net/http"
+import (
 	"html/template"
 	"log"
+	"net/http"
+)
+
+type indexPage struct {
+	Title         string
 }
 
-type indexPageData struct {
-	Title string
-	Subtitle string
+type postPage struct {
+	Title         string
 }
 
-
-func index(w http.ResponseWriter, r *http.Request) { // Функция для отдачи страницы
+func index(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
 	if err != nil {
 		http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-		log.Println(err.Error()) // Используем стандартный логгер для вывода ошибки в консоль
-		return // Не забываем завершить выполнение ф-ии
+		log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+		return                                      // Не забываем завершить выполнение ф-ии
 	}
- 
-   // Подготовим данные для шаблона
-   data := indexPageData {
-	Title: "Blog for traveling",
-	Subtitle: "My best blog"
-   }
- 
-	err = ts.Execute(w, nil) // Запускаем шаблонизатор для вывода шаблона в тело ответа
+
+	data := indexPage{
+		Title:         "Escape",
+	}
+
+	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
 	if err != nil {
 		http.Error(w, "Internal Server Error", 500)
+		log.Println(err.Error())
 		return
-	} 
- }
- 
+	}
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("pages/post.html") // Главная страница блога
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+		log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+		return                                      // Не забываем завершить выполнение ф-ии
+	}
+
+	data := postPage{
+		Title:         "Escape",
+	}
+
+	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500)
+		log.Println(err.Error())
+		return
+	}
+}
+
--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,20 +1,21 @@
 package main
 
-import {
+import (
 	"log"
 	"net/http"
-}
+)
 
 const port = ":3000"
 
 func main() {
 	mux :=http.NewServeMux()
 	mux.HandleFunc("/home", index)
-
+	mux.HandleFunc("/post", post)
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	log.Println("Start server")
 	http.ListenAndServe(port, mux)
+	log.Println("Start server")
 }
 
  
